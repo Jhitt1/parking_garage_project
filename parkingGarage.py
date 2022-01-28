1cbd14f5596fe6f91afc6fa91e55a39f2367c453
Justin Rittenhouse <rittenhouse.justin@gmail.com>
2022-01-28 16:12:16 -0500

    test_push

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,7 +1,7 @@
 import random
 
 class Garage():
-    def __init__(self, tickets, parkingSpaces, currentTicket):
+    def __init__(self, tickets, parkingSpaces, currentTicket = {'ticket_number': 0, 'parking_space_number': 0, 'paid': False}):
         self.tickets = tickets
         self.parkingSpaces = parkingSpaces
         self.currentTicket = currentTicket
@@ -11,7 +11,8 @@ class Garage():
 
 
 
+
 tickets = []
 for i in range(100):
     tickets.append(i + 1)
-garage = Garage([], [], {})
+garage = Garage(tickets, [], {})
