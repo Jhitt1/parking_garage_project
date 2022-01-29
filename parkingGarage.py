0ccba81a3395c29c43b8a898d49ee97219b496fd
Jesse Hitt <hittj77@gmail.com>
2022-01-29 14:19:23 -0600

    take_ticket

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,4 +1,8 @@
 import random
+from datetime import datetime
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
 
 class Garage():
     def __init__(self, tickets, parkingSpaces, currentTicket = {'ticket_number': 0, 'parking_space_number': 0, 'paid': False}):
@@ -6,12 +10,28 @@ class Garage():
         self.parkingSpaces = parkingSpaces
         self.currentTicket = currentTicket
     
+    def takeTicket(self):
+        temp_ticket_number =  random.choice(self.tickets)
+        self.currentTicket["ticket_number"] = temp_ticket_number
+        self.tickets.remove(temp_ticket_number)
+        temp_parkingSpaces = random.choice(self.parkingSpaces)
+        self.currentTicket["parking_space_number"] = temp_parkingSpaces
+        self.parkingSpaces.remove(temp_parkingSpaces)
+        inout("\nThe current time is " + current_time)
+        input("\nYour parking space is " + str(temp_parkingSpaces))
+        input("\nYour ticket number is " + str(temp_ticket_number))
+        input("\nPlease pay for your ticket once your ready to leave.")
+
+
+
+
     def payForParking(self):
        payment =  input("\nHow much are you paying for your ticket?")
 
 
 
 
+# justin section start
 tickets = []
 parkingSpaces = []
 for i in range(100):
